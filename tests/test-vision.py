0da8783e18631ca74c2dd5aec2ccc3f61975a324
stars-n-stripes <22804241+stars-n-stripes@users.noreply.github.com>
2022-10-11 14:33:57 -0500

    Trimmed print statements

--- tests/test-vision.py
+++ tests/test-vision.py
@@ -11,21 +11,15 @@ image = cv2.imread(multi_qr_imgpath)
 detector = cv2.QRCodeDetector()
 
 retval, decodedTexts, points, _ = detector.detectAndDecodeMulti(image)
-print(retval)
 
 # decodedText, points, _ = detector.detectAndDecode(image)
-if points is not None:
+if retval and points is not None:
     for i in range(points.shape[0]):
         qr_point_group = points[i,:]
-        print(qr_point_group)
         for j in range(len(qr_point_group)):
-            print(qr_point_group[j].shape)
             next_idx = (j+1) % len(qr_point_group)
-            print(tuple(qr_point_group[j]))
-            print(tuple(qr_point_group[next_idx]))
             cv2.line(image, tuple(qr_point_group[j].astype(int)), tuple(qr_point_group[next_idx].astype(int)), (255, 0, 0), 5)
-            # cv2.line(image, tuple(points[i][0]), tuple(points[nextPointIndex][0]), (255,0,0), 5)
-        print(decodedTexts[i])
+        print(f"Detected Code: {decodedTexts[i]}")
 
 cv2.imshow("Image", image)
 cv2.waitKey(0)
