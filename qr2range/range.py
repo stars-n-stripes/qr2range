4ec03f6f55116c0dd005962994cca73b21b9e415
stars-n-stripes <22804241+stars-n-stripes@users.noreply.github.com>
2022-10-12 15:43:15 -0500

    Added skeletons for range and scenario objects

---
+++ qr2range/range.py
@@ -0,0 +1,12 @@
+class BaseRange:
+    def __init__(self) -> None:
+        pass
+    
+class BaseNode:
+    def __init__(self) -> None:
+        pass
+    
+class BaseNetwork:
+    def __init__(self) -> None:
+        pass
+ 
